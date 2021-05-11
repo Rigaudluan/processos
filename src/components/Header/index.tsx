02a829a03c2d1ce52e15bad0c9164670a2c5f11f
theuslefone <matheuskeeven22@gmail.com>
2021-05-10 23:34:18 -0300

    Finaliza Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,9 @@ export function Header(){
     return(
         <header className={styles.headerContainer}>
             <nav>
-                <img src="" alt="Cesar Process" />
-                <a href="#">INÍCIO</a>
-                <a href="#">PROCESSOS</a>
+                <img src="/logo.svg" alt="Cesar Process" />
+                <a href="#" className = {styles.selected}> Início </a>
+                <a href="#">Prcessos</a>
             </nav>
         </header>
     );
