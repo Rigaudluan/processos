7a4647f8ebf517386cb1b7a6b06f5299316d35f0
theuslefone <matheuskeeven22@gmail.com>
2021-05-10 22:46:11 -0300

    atualiza

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from './styles.module.scss'
+
 export function Header(){
     return(
-        <header>
+        <header className={styles.headerContainer}>
             <nav><ul>
                 <li><img src="" alt="Cesar Process" /></li>
                 <li><a href="#">INÍCIO</a></li>
