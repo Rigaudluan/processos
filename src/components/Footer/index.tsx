11122990e565d19fba722c65aae6f1cf9a2ba36e
Rigaudluan <rigaudluan@gmail.com>
2021-05-12 15:57:02 -0300

    Atualiza footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <footer className={styles.footerContainer}>
             <div className={styles.footerObject1}><p>© Copyright 2021-2021 CESAR Processos Developed by Programadores HTML</p></div>
-            <div className={styles.footerObject2}><p>Algum problema?<a href="#">Relate aqui!</a></p></div>
+            <div className={styles.footerObject2}><p>Algum problema? <a href="#"><b>Relate aqui!</b></a></p></div>
         </footer>
     );
 }
