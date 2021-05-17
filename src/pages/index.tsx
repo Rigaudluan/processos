1c587859566f51b00fa01fd1b3e12e2336b81d83
theuslefone <matheuskeeven22@gmail.com>
2021-05-17 12:16:53 -0300

    importa PrincipalProcess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import { Header } from "../components/Header/index";
 import { Footer } from "../components/Footer/index";
 import { SearchInput } from "../components/SearchInput/index";
+import React from "react";
+import PrincipalProcess from "../components/PrincipalProcess";
 
-export default function Home() {
+
+export default function Home(props) {
   return (
     <body>
       <Header />
       <main>
+      <PrincipalProcess />
       <SearchInput />
       </main>
       <Footer />
     </body>
   )
 }
+
+
