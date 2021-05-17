bf1e98fde32ddaed57c7135d4d8a063f9a5a2b03
theuslefone <matheuskeeven22@gmail.com>
2021-05-17 12:43:42 -0300

    importa com AXIOS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,22 @@ import { Header } from "../components/Header/index";
 import { Footer } from "../components/Footer/index";
 import { SearchInput } from "../components/SearchInput/index";
 import React from "react";
-import PrincipalProcess from "../components/PrincipalProcess";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+
+type Process = {
+  id: string,
+  title: string,
+  area: string,
+  latest_modification: string,
+  thumbnail: string,
+  description: string
+}
+
+type HomeProps = {
+  PrincipalProcesses: Process[],
+  RecomendedProcesses: Process[]
+}
 
 
 export default function Home(props) {
@@ -10,12 +25,31 @@ export default function Home(props) {
     <body>
       <Header />
       <main>
-      <PrincipalProcess />
       <SearchInput />
+      <p> {JSON.stringify(props.processes)}</p>
       </main>
       <Footer />
     </body>
   )
 }
+
+export const getStaticProps: GetStaticProps = async() => {
+  const { data } = await api.get('processes', {
+    params:{
+    _limit: 12,
+    _sort: 'id',
+    _order: 'desc'
+    }
+
+  })
+
+  return{
+    props: {
+      processes: data,
+    },
+    revalidate: 60 * 10
+  }
+}
+
 
 
