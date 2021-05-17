65f10093db92d994d78e222c88617f9616ec498c
theuslefone <matheuskeeven22@gmail.com>
2021-05-17 16:30:55 -0300

    cria modais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { SearchInput } from '../components/SearchInput'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <div>
+    <Header />
+      <main>
+      <SearchInput />
+     <Component {...pageProps} />
+     </main>
+      <Footer />
+   </div>
+  )
 }
+
 
 export default MyApp
