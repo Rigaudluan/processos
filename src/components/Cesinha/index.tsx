fd4983ba0dc3dc2d0d1eb249319a43f4ec211b66
Lorena Araújo <lorena02.lv@gmail.com>
2021-05-18 17:26:56 -0300

    adiciona pacote chatbot

--- src/components/Cesinha/index.tsx
+++ src/components/Cesinha/index.tsx
@@ -1,7 +1,15 @@
-export default function Cesinha(){
-    return(
-        <div>
-               
-        </div>
-    )
+import styles from './styles.module.scss'
+
+export default function Cesinha() {
+    return (
+            <div className={styles.cesinhaContainer}>
+                <div className={styles.icon}></div>
+                <div className={styles.messageBox}>
+                    <div className={styles.messagePopup}>
+                        <p className={styles.messageText}>Qualquer coisa fala comigo, ok?</p>
+                        <p className={styles.messageTime}>10:00</p>
+                    </div>
+                </div>
+            </div>
+    )
 }
