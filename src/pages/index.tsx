39ba395f3d4e0c9f9b8cbfaa72a8ba3197821f39
Rigaudluan <rigaudluan@gmail.com>
2021-05-17 22:32:39 -0300

    atualiza

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,20 +16,40 @@ type Processo = {
 }
 
 type HomeProps = {
-  PrincipalProcesses: Processo[],
-  RecomendedProcesses: Processo[]
+  mostSearchedProcesses: Processo[],
+  exclusiveProcesses: Processo[]
 }
 
 
-export default function Home(props) {
+export default function Home({ mostSearchedProcesses, exclusiveProcesses }) {
   return (
     <body>
       <div className={styles.processesShowcase}>
         <section className={styles.mostSearchedProcesses}>
           <h2>Processos mais pesquisados</h2>
+
+          <ul>
+            {mostSearchedProcesses.map(Processo => {
+              return (
+                <li>
+                  <a href="">{Processo.title}</a>
+                </li>
+              )
+            })}
+          </ul>
         </section>
         <section className={styles.exclusiveProcesses}>
           <h2>Excluisvos para você</h2>
+
+          <ul>
+            {exclusiveProcesses.map(Processo => {
+              return (
+                <li>
+                  <a href="">{Processo.title}</a>
+                </li>
+              )
+            })}
+          </ul>
         </section>
       </div>
     </body>
@@ -56,9 +76,13 @@ export const getStaticProps: GetStaticProps = async () => {
     };
   })
 
+  const mostSearchedProcesses = Processo.slice(0, Processo.length);
+  const exclusiveProcesses = Processo.slice(0, Processo.length);
+
   return {
     props: {
-      Processo
+      mostSearchedProcesses,
+      exclusiveProcesses,
     },
     revalidate: 60 * 10
   }
