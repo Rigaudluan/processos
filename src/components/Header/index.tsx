17437788276d098ae07ba3dd715f815def23e399
theuslefone <matheuskeeven22@gmail.com>
2021-05-20 14:29:05 -0300

    adiciona rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export function Header(){
     return(
         <header className={styles.headerContainer}>
-                <a href=""> <Image src="/logo.png" width={100} height={100} /> </a>
-                <a href="#" className = {styles.selected}> Início </a>
-                <a href="#" className ={styles.off} >Processos</a>
+                <a href={`/`}> <Image src="/logo.png" width={100} height={100} /> </a>
+                <a href={`/`} className = {styles.selected}> Início </a>
+                <a href={`/paginadeprocessos`} className ={styles.off} >Processos</a>
         </header>
     );
 }
