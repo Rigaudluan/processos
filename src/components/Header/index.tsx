05fbc8128bf369809adbe19bf3ea3f924ec824d4
theuslefone <matheuskeeven22@gmail.com>
2021-05-21 11:58:45 -0300

    refurmula Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header(){
     return(
         <header className={styles.headerContainer}>
             <Link href={`/`}>
-                <a> <Image src="/logo.png" width={100} height={100} /> </a>
+                <a> Cesar Processos </a>
             </Link>
             <Link href={`/`}>
                  <a  className = {styles.selected}> Início </a>
@@ -14,7 +14,6 @@ export function Header(){
             <Link href={`/paginadeprocessos`}>
                  <a  className ={styles.off} >Processos</a>
             </Link>
-               
         </header>
     );
 }
