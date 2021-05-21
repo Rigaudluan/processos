766fa87c4ca356fa0e126c0c286f8373b4c9b53c
theuslefone <matheuskeeven22@gmail.com>
2021-05-21 11:58:04 -0300

    refurmula Header

--- src/pages/processos/[slug].tsx
+++ src/pages/processos/[slug].tsx
@@ -1,4 +1,6 @@
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { api } from "../../services/api";
 
 export default function processoAberto(){
     
@@ -6,4 +8,18 @@ export default function processoAberto(){
     return(
         <h1>{router.query.slug}</h1>
     )
+
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params;
+    const {data} = await api.get(`/processos/${slug}`)
+
+    return {
+      props: {
+
+      },
+      revalidate: 60 * 10
+    }
+  }
+  
