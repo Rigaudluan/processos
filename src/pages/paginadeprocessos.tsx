7d8a95e5de94be410c50bf7c2a37687f268c9398
theuslefone <matheuskeeven22@gmail.com>
2021-05-21 21:16:14 -0300

    finaliza página de processos

--- src/pages/paginadeprocessos.tsx
+++ src/pages/paginadeprocessos.tsx
@@ -1,24 +1,68 @@
 import { GetStaticProps, GetStaticPaths } from "next";
+import styles from './home.module.scss';
 import { SearchInput } from "../components/SearchInput";
 import { TitleProcess } from "../components/TitleProcess/titleProcess";
 import { api } from "../services/api";
+import Link from "next/link";
+import Image from 'next/image';
 
-type Processo = {
+type TodosProcessos = {
     id: string,
     title: string,
-    area: string,
-    latest_modification: string,
     thumbnail: string,
     description: string
-  }
+}
+type ProcessoProps = {
+    processos : TodosProcessos[];
+}
   
 
-export default function paginadeprocessos(){
+export default function paginadeprocessos({processos }: ProcessoProps){
     return(
         <main>
             <TitleProcess name='Processos'/>
             <SearchInput />
 
+            <ul className={styles.principalContainer2}>
+            {processos.map(processo => {
+                return(
+                    <div key={processo.id}>
+                        <img src={processo.thumbnail} alt="" />
+                        <Link href={`/processos/${processo.id}`}>
+                            <a >{processo.title}
+                            <p>{processo.description}</p></a>
+                        </Link>
+                     </div>
+                )
+            })}
+            </ul>
         </main>
     )
+}
+
+export const getStaticProps : GetStaticProps = async () => {
+ const { data } = await api.get('processos', {
+     params:{
+         _limit: 12,
+         _sort: 'id',
+         _order: 'desc'
+     }
+ })
+
+ const todososprocessos = data.map(TodosProcessos => {
+    return{
+        id: TodosProcessos.id,
+        title: TodosProcessos.title,
+        thumbnail: TodosProcessos.thumbnail,
+        description: TodosProcessos.description
+        
+    }
+ })
+
+    return{
+        props:{
+            processos: data,
+        },
+        revalidate: 60 * 10
+    }  
 }
