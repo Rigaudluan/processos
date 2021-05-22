d4ac505b39cd901ec136b3db2b12344c42ee0bf5
theuslefone <matheuskeeven22@gmail.com>
2021-05-21 22:03:16 -0300

    Termina Header com logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 export function Header(){
     return(
         <header className={styles.headerContainer}>
             <Link href={`/`}>
-                <a> Cesar Processos </a>
+                <a className={styles.imageDetail}>
+                <Image 
+                src="/logo.png"
+                width={65}
+                height={65}
+                />
+                </a>
             </Link>
             <Link href={`/`}>
                  <a  className = { styles.selected }> Início </a>
