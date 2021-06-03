c55d5965907e5300d02f25230ece0e4aa35cdf9f
theuslefone <matheuskeeven22@gmail.com>
2021-06-03 11:09:14 -0300

    adiciona funções pra identificar página

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,42 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
-import Image from 'next/image';
+import Image from 'next/image';;
 
+var page = true
+
+
+function initialPage(){
+    page = true
+}
+
+function pageProcess(){
+    page = false
+}
 
 export function Header(){
     return(
         <header className={styles.headerContainer}>
+           <button onClick={initialPage} >
             <Link href={`/`}>
-                <a className={styles.imageDetail}>
-                <Image 
-                src="/logo.png"
-                width={65}
-                height={65}
-                />
-                </a>
-            </Link>
-            <Link href={`/`}>
-                 <a  className = { styles.selected }> Início </a>
-            </Link>
-            <Link href={`/paginadeprocessos`}>
-                 <a className ={ styles.off } >Processos</a>
-            </Link>
+                    <a className={styles.imageDetail}>
+                    <Image 
+                    src="/logo.png"
+                    width={65}
+                    height={65}
+                    />
+                    </a>
+                </Link>
+           </button>
+           <button onClick={initialPage}>
+                <Link href={`/`}>
+                    <a className =  { page? styles.selected : styles.off }> Início </a>
+                </Link>
+           </button>
+            <button onClick={pageProcess}>
+                <Link href={`/paginadeprocessos`}>
+                    <a className = { page? styles.off  : styles.selected} >Processos</a>
+                </Link>
+            </button>
         </header>
     );
 }
