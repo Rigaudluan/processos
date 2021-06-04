77ca79004c89d17c4c8d5c7145bb4a754e01d21e
theuslefone <matheuskeeven22@gmail.com>
2021-06-04 17:50:05 -0300

    mostra os arquivos de dentro

--- src/pages/processo/[nomeDoProcesso].tsx
+++ src/pages/processo/[nomeDoProcesso].tsx
@@ -1,10 +1,70 @@
-import {useRouter} from 'next/router'
+import { GetStaticProps, GetStaticPaths } from "next";
+import styles from './process.module.scss';
+import { SearchInput } from "../../components/SearchInput";
+import { TitleProcess } from "../../components/TitleProcess/titleProcess";
+import { api } from "../../services/api";
+import Link from "next/link";
 
-export default function Process(){
+type TodosProcessos = {
+    id: string,
+    name: string,
+    iconLink: string
+}
 
-  const router = useRouter();
+type ProcessoProps = {
+    processos : TodosProcessos[];
+}
+  
 
-  return(
-      <p>{router.query.nomeDoProcesso}</p>
-  )
+export default function paginadeprocessos({processos }: ProcessoProps){
+    return(
+        <main>
+            <ul >
+            {processos.map(processo => {
+                return(
+                    <div className={styles.principalContainer2} key={processo.id}>
+                        <div className={styles.img}>
+                        <img src={processo.iconLink} alt={processo.name}/>
+                        </div>
+                        <div>
+                        <Link href={`processo/${processo.id}`}>
+                            <a>{processo.name}</a>
+                        </Link>
+                     </div>
+                    </div>
+                )
+            })}
+            </ul>
+        </main>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps : GetStaticProps = async (ctx) =>{
+
+  const {nomeDoProcesso} = ctx.params;
+
+  const {data} = await api.get(`/processo/${nomeDoProcesso}`)
+ 
+
+  const processo = data.files.map(process =>{
+        return{
+          id: process.id,
+          name: process.name,
+          iconLink: process.iconLink
+        }
+      }
+    )
+    return{
+        props:{
+            processos: data.files,
+        },
+        revalidate: 60 * 10
+    }  
 }
