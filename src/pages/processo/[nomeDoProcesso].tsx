91fbfd25bad8e1df215601bc909da1c671f54a28
Rigaudluan <rigaudluan@gmail.com>
2021-06-04 23:58:53 -0300

    modifica estrutura

--- src/pages/processo/[nomeDoProcesso].tsx
+++ src/pages/processo/[nomeDoProcesso].tsx
@@ -4,6 +4,7 @@ import { SearchInput } from "../../components/SearchInput";
 import { TitleProcess } from "../../components/TitleProcess/titleProcess";
 import { api } from "../../services/api";
 import Link from "next/link";
+import Image from "next/image";
 
 type TodosProcessos = {
     id: string,
@@ -12,58 +13,70 @@ type TodosProcessos = {
 }
 
 type ProcessoProps = {
-    processos : TodosProcessos[];
+    processos: TodosProcessos[];
 }
-  
 
-export default function paginadeprocessos({processos }: ProcessoProps){
-    return(
-                <main>
-                    <ul >
-                        {processos.map(processo => {
-                            return(
-                                <div className={styles.principalContainer2} key={processo.id}>
-                                    <div className={styles.img}>
-                                    <img src={processo.iconLink} alt={processo.name}/>
-                                    </div>
-                                    <div>
+
+export default function paginadeprocessos({ processos }: ProcessoProps) {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <button>
+                    <img src="/arrow-left.svg" alt="voltar" />
+                </button>
+            </header>
+            <div className={styles.nomeProcess}>
+
+            </div>
+            <main className={styles.main}>
+                <ul >
+                    {processos.map(processo => {
+                        return (//<Image src="/arrow-left.svg" className={styles.goBack} width={28} height={28}/>
+
+                            <div className={styles.process} key={processo.id}>
+                                <div className={styles.image}>
+                                    <img src={processo.iconLink} alt={processo.name} />
+                                </div>
+                                <div>
                                     <Link href={`processo/${processo.id}`}>
                                         <a>{processo.name}</a>
                                     </Link>
                                 </div>
-                                </div>
+                            </div>
                         )
-                    }
-                )
-            }
-            </ul>
-        </main>
+                    })}
+                </ul>
+            </main>
+        </div>
     )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: 'blocking'
-  }
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
 }
 
-export const getStaticProps : GetStaticProps = async (ctx) =>{
+export const getStaticProps: GetStaticProps = async (ctx) => {
 
-  const {nomeDoProcesso} = ctx.params;
-  const {data} = await api.get(`/processo/${nomeDoProcesso}`);
-  const processo = data.files.map(process =>{
-        return{
-          id: process.id,
-          name: process.name,
-          iconLink: process.iconLink
+    const { nomeDoProcesso } = ctx.params;
+
+    const { data } = await api.get(`/processo/${nomeDoProcesso}`)
+
+
+    const processo = data.files.map(process => {
+        return {
+            id: process.id,
+            name: process.name,
+            iconLink: process.iconLink
         }
-      }
+    }
     )
-    return{
-        props:{
+    return {
+        props: {
             processos: data.files,
         },
         revalidate: 60 * 10
-    }  
+    }
 }
