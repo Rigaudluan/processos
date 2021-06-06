d645397c8a77e7c9c53e4413ee4e345e0b1ae4de
theuslefone <matheuskeeven22@gmail.com>
2021-06-06 15:46:36 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const linkApi = 'http://b2221523b12d.ngrok.io'
+export const linkApi = 'http://86180378b6e5.ngrok.io'
 
 export const api = axios.create({
     baseURL: linkApi
