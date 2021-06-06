9d44e44daa3aa0fb35593aeaeb00d45a66815ed5
Enrico <Matheusx1272@gmail.com>
2021-06-06 18:48:52 -0300

    tentando mostrar em tela

--- src/pages/pesquisa.tsx
+++ src/pages/pesquisa.tsx
@@ -1,7 +1,33 @@
-export default function SearchPage() {
+import { api } from "../services/api"
+
+export default function pesquisa() {
+    const route = useRoute<RouteProp<UserParamList,"Pesquisa">>();
+    
+    
     return(
         <div>
             
         </div>
     )
+}
+
+export default function Home(){
+    
+
+constdefaultPosts:IPost[]=[];const[posts,setPosts]=useState<IPost[]>(defaultPosts);
+
+
+async function onSearchProcesses(props){
+    let results = props
+    const {data} = await api.get(`pesquisa/${results}`)
+    console.log(data.files)
+  
+const processes = data.files.map(processes => {
+    return{
+        id: processes.id,
+        name: processes.name,
+        iconLink: processes.iconLink
+    }
+  }
+)
 }
