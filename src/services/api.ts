27e54c845f552dc129265ac8c46a8e21d7928912
Rigaudluan <rigaudluan@gmail.com>
2021-06-06 11:41:21 -0300

    muda link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const linkApi = 'http://950565df623f.ngrok.io'
+export const linkApi = 'hhttp://9d86f2545b6c.ngrok.io'
 
 export const api = axios.create({
     baseURL: linkApi
