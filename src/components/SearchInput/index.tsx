a7aecbf0a94011cce4ccb92503e3fa4caf590404
Enrico <Matheusx1272@gmail.com>
2021-06-07 22:26:15 -0300

    Incrimentando SearchInput

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -3,9 +3,39 @@ import Formik, { useFormik } from 'formik';
 import styles from './styles.module.scss';
 import { api } from '../../services/api';
 import Link from 'next/link';
+import { timeStamp } from 'console';
 
 
+type processes = {
+  id: string,
+  name: string,
+  iconLink: string,
+  webViewLink: string
+  
+}
 export function SearchInput() {
+
+  const [image, setImage] = useState(null)
+  const [id, setId] = useState(null)
+  const [name, setName] = useState(null)
+  const [tempProcesses, setTempProcesses] = useState(null)
+
+  async function onSearchProcesses(props) {
+    let results = props
+    const { data } = await api.get(`pesquisa/${results}`)
+    let resultado = { id: "10RzPg3BurxA1FwpKnQ_JTrO5tZPoe6iF", name: "Cesar School", mimeType: "application/vnd.google-apps.folder", webViewLink: "https://drive.google.com/drive/folders/10RzPg3BurxA1FwpKnQ_JTrO5tZPoe6iF", iconLink: "https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.folder+shared" }
+
+    const processes : processes = data
+    setName(processes.name)
+    setId(processes.id)
+    setImage(processes.iconLink)
+
+    return {
+      processes: processes
+    }
+  }
+  console.log(tempProcesses)
+
   function useFormik({
     initialValues,
   }) {
@@ -34,46 +64,28 @@ export function SearchInput() {
   return (
     <div>
       <form onSubmit={(event) => {
-      event.preventDefault();
-      onSearchProcesses(formik.values.userText)
-    }}
-    >
+        event.preventDefault();
+        onSearchProcesses(formik.values.userText)
+      }}
+      >
 
-      <div className={styles.siContainer}>
-        <input
-          className={styles.siInput}
-          type="text"
-          placeholder="Ex. : Reembolso"
-          name="userText"
-          id="pesquisa"
-          onChange={formik.handleChange}
-          value={formik.values.userText}
-        />
-        <button type="submit">
-          pesquisar
+        <div className={styles.siContainer}>
+          <input
+            className={styles.siInput}
+            type="text"
+            placeholder="Ex. : Reembolso"
+            name="userText"
+            id="pesquisa"
+            onChange={formik.handleChange}
+            value={formik.values.userText}
+          />
+          <button type="submit">
+            pesquisar
         </button>
-        <p></p>
-      </div>
-    </form>
+          <p>{tempProcesses}</p>
+        </div>
+      </form>
     </div>
   );
 }
 
-async function onSearchProcesses(props){
-  let results = props
-  const {data} = await api.get(`pesquisa/${results}`)
-  console.log(data.files)
-
-const processes = data.files.map(processes => {
-      return{
-          id: processes.id,
-          name: processes.name,
-          iconLink: processes.iconLink
-      }
-    }
-  )
-
-  return{
-    processes: processes
-  }
-}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const linkApi = 'http://eec4ab6f1e75.ngrok.io'
+export const linkApi = 'http://676d8cd5ad58.ngrok.io'
 
 export const api = axios.create({
     baseURL: linkApi
