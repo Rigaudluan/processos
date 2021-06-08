6146c0c36a7f670cfbf0810f6b15dcb5ecadc18d
Rigaudluan <rigaudluan@gmail.com>
2021-06-08 01:55:04 -0300

    formata a pagina principal para mostrar os processos listados e a pesquisa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,69 @@ import { api } from "../services/api";
 import { SearchInput } from "../components/SearchInput";
 import { TitleProcess } from "../components/TitleProcess/titleProcess";
 
+type TodosProcessos = {
+  id: string,
+  name: string,
+  iconLink: string
+}
 
-export default function Home(){
+type ProcessoProps = {
+  processos: TodosProcessos[];
+}
+
+export default function Home({ processos }: ProcessoProps){
 
   return (
     <main>
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <TitleProcess name='Página Principal' />
-      <SearchInput />
+            <TitleProcess name='Seja bem-vindx!' />
+            <div className={styles.mainContainer}>
+            <div className={styles.searchInputContainer}>
+              <h2>Faça sua busca </h2>
+              <SearchInput />
+              </div>
+              <hr />
+            <div className={styles.processesContainer}><ul >
+                {processos.map(processo => {
+                    return (
+                        <div className={styles.containerGeral}>
+                            <div></div>
+                            <div className={styles.principalContainer2} key={processo.id}>
+                                <div className={styles.img}>
+                                    <img src={processo.iconLink} alt={processo.name} />
+                                </div>
+                                <div>
+                                    <Link href={`processo/${processo.id}`}>
+                                        <a>{processo.name}</a>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div></div>
+                        </div>
+                    )
+                })}
+            </ul> </div>  
+            </div>            
     </main>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('files')
+
+  const todososprocessos = data.files.map(TodosProcessos => {
+      return {
+          id: TodosProcessos.id,
+          name: TodosProcessos.name,
+          iconLink: TodosProcessos.iconLink
+      }
+  })
+
+  return {
+      props: {
+          processos: data.files,
+      },
+      revalidate: 60 * 10
+  }
 }
 
 
