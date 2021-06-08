128f0f02eeef5c538a7d758a87f285af1be0dc7a
Rigaudluan <rigaudluan@gmail.com>
2021-06-08 03:39:15 -0300

    codigo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const linkApi = 'http://1398071eec4b.ngrok.io'
+export const linkApi = 'http://9e9e793b8200.ngrok.io'
 
 export const api = axios.create({
     baseURL: linkApi
